45084161c70fa043adb6c4915f4f1cbc739c0fff
William Lahti <wilahti@gmail.com>
2019-02-23 00:35:37 -0800

    add docs to test exec settings

--- src/core/test-execution-settings.ts
+++ src/core/test-execution-settings.ts
@@ -1,7 +1,25 @@
 export interface TestExecutionSettingsDef {
+    /**
+     * Maximum amount of time (in milliseconds)
+     * that tests can run. If they run longer, 
+     * they are considered a failure.
+     */
     timeout? : number;
     contextName? : string;
+
+    /**
+     * Define the order to run the tests.
+     * Use 'default' for load order, and 
+     * 'random' to randomly order the tests.
+     * See `orderSeed`.
+     */
     order? : 'default' | 'random';
+
+    /**
+     * Specify the seed to use to order the 
+     * tests for execution. Leave undefined 
+     * to have a seed chosen at runtime.
+     */
     orderSeed? : number;
 }
 
