75c9c5e7480f41eaeb56a053ddb7d2164f03f709
William Lahti <wilahti@gmail.com>
2020-08-22 14:28:07 -0700

    fix: ReferenceError: __razminGlobalSuiteSettings is not defined

--- src/core/global-settings.ts
+++ src/core/global-settings.ts
@@ -3,5 +3,7 @@ import { SuiteSettings } from "./suite-settings";
 declare var __razminGlobalSuiteSettings : SuiteSettings;
 
 export function globalSuiteSettings() : SuiteSettings {
-    return __razminGlobalSuiteSettings;
+    if (typeof __razminGlobalSuiteSettings !== 'undefined')
+        return __razminGlobalSuiteSettings;
+    return null;
 }
